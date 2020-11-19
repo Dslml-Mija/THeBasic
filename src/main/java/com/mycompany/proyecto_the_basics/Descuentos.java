444d74f6373ffa08c88e046fa08d9933ebfd9df0
mija2 <mija2@DESKTOP-8M1MM1U>
2020-11-19 00:17:28 -0600

    descuentos

---
+++ src/main/java/com/mycompany/proyecto_the_basics/Descuentos.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.proyecto_the_basics;
+
+/**
+ *
+ * @author mija2
+ */
+public class Descuentos {
+    
+    public int Desc1;
+    public int Desc2;
+    public int Desc3;
+    public int Desc4;
+    public int Desc5;
+
+    public int getDesc1() {
+        return Desc1;
+    }
+
+    public void setDesc1(int Desc1) {
+        this.Desc1 = Desc1;
+    }
+
+    public int getDesc2() {
+        return Desc2;
+    }
+
+    public void setDesc2(int Desc2) {
+        this.Desc2 = Desc2;
+    }
+
+    public int getDesc3() {
+        return Desc3;
+    }
+
+    public void setDesc3(int Desc3) {
+        this.Desc3 = Desc3;
+    }
+
+    public int getDesc4() {
+        return Desc4;
+    }
+
+    public void setDesc4(int Desc4) {
+        this.Desc4 = Desc4;
+    }
+
+    public int getDesc5() {
+        return Desc5;
+    }
+
+    public void setDesc5(int Desc5) {
+        this.Desc5 = Desc5;
+    }
+    
+
+    public Descuentos(int Desc1, int Desc2, int Desc3, int Desc4, int Desc5) {
+        this.Desc1 = Desc1;
+        this.Desc2 = Desc2;
+        this.Desc3 = Desc3;
+        this.Desc4 = Desc4;
+        this.Desc5 = Desc5;
+    }
+    
+}
