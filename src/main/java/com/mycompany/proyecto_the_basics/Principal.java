606ee7b355be87e83c8f2f79af08ba9876945144
mija2 <mija2@DESKTOP-8M1MM1U>
2020-11-21 08:24:27 -0600

    Se valida la informacion ya dada en la clase usuario

--- src/main/java/com/mycompany/proyecto_the_basics/Principal.java
+++ src/main/java/com/mycompany/proyecto_the_basics/Principal.java
@@ -19,27 +19,21 @@ public class Principal {
     public static void main(String[] args) {
         // TODO code application logic here
             int menu;
-            
-            Usuario inicio =new Usuario();
-            inicio.getListaUsuarios();
-                    
-                    
-            do{
-                JOptionPane.showMessageDialog(null, "Bienvenido a The Basic Store");
+            JOptionPane.showMessageDialog(null, "Bienvenido a The Basic Store");
+                do{
+                
                 menu= Integer.parseInt(JOptionPane.showInputDialog("Digite el numero de la accion que desea ralizar \n"+"1.Usuario \n2.Registro \n3.Administrador \n0.Salir"));
                 if(menu==1){
                     Usuario logIn=new Usuario();
                     logIn.LogIn();
+                    
                 }
                 if(menu==2){
                     Usuario registro= new Usuario();
                     registro.nuevoRegistro();
                     registro.LogIn();
                 }
-                if(menu==3){
-                    Adminitracion log =new Adminitracion();
-                    log.logAdmin();
-                }
+                
                 
             }while(menu!=0);
     }
